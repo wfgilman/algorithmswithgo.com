47060666a93a8cd6946c509de45a5e5cef1af811
Will Gilman <will@samadhi.tech>
2022-04-20 14:39:35 -0700

    Base to Decimal conversion

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -9,5 +9,35 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	val := Reverse(value)
+	res := 0
+	mult := 1
+	for _, num := range val {
+		n := toDec(string(num))
+		res += n * mult
+		mult *= base
+	}
+	return res
+}
+
+func toDec(val string) int {
+	var charmap = map[string]int{
+		"0": 0,
+		"1": 1,
+		"2": 2,
+		"3": 3,
+		"4": 4,
+		"5": 5,
+		"6": 6,
+		"7": 7,
+		"8": 8,
+		"9": 9,
+		"A": 10,
+		"B": 11,
+		"C": 12,
+		"D": 13,
+		"E": 14,
+		"F": 15,
+	}
+	return charmap[val]
 }
