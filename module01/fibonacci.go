5675e57c0cd124f4022245a9a64b8400aa674fca
Will Gilman <will@samadhi.tech>
2022-04-20 21:04:02 -0700

    Fib algo

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -25,5 +25,19 @@ package module01
 //   Fibonacci(14) => 377
 //
 func Fibonacci(n int) int {
-	return 0
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	nm2 := 0
+	nm1 := 1
+	var fib int
+	for i := 2; i <= n; i++ {
+		fib = nm1 + nm2
+		nm2 = nm1
+		nm1 = fib
+	}
+	return fib
 }
